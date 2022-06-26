d016e0df88608048b8f9dfdf174f99799dc2a80d
Я <@DESKTOP-MI55O4C>
2022-06-26 14:54:27 +0300

    amendments point

--- IntroductionToOOP/Point.cpp
+++ IntroductionToOOP/Point.cpp
@@ -1,13 +1,7 @@
 #include "Point.h"
 
-class Point
-{
-	double x;
-	double y;
 
-public:
-
-	Point& operator= (const Point& other)
+	Point& Point::operator= (const Point& other)
 	{
 		this->x = other.x;
 		this->y = other.y;
@@ -15,20 +9,20 @@ public:
 		return *this;
 	}
 
-	double get_x()const
+	double Point::get_x()const
 	{
 		return x;
 	}
-	double get_y()const
+	double Point::get_y()const
 	{
 		return y;
 	}
 
-	void set_x(double x)
+	void Point::set_x(double x)
 	{
 		this->x = x;
 	}//
-	void set_y(double y)
+	void Point::set_y(double y)
 	{
 		this->y = y;
 	}
@@ -52,36 +46,34 @@ public:
 		cout << "Constructor: " << this << endl;
 	}
 	*/
-	Point(double x = 0, double y = 0)//есть возможность заменить все конструкторы выше на такой. но с ним нужно быть аккуратным!
+	Point::Point(double x = 0, double y = 0)//есть возможность заменить все конструкторы выше на такой. но с ним нужно быть аккуратным!
 	{
 		this->x = x;
 		this->y = y;
 		cout << "Constructor: " << this << endl;
 	}
-	Point(const Point& other)
+	Point::Point(const Point& other)
 	{
 		this->x = other.x;
 		this->y = other.y;
 		cout << "CopyConstructor: " << this << endl;
 	}
-	~Point()
+	Point::~Point()
 	{
 		cout << "Destructor: " << this << endl;
 	}//
 
-	double distance(const Point& other)
+	double Point::distance(const Point& other)
 	{
 		double distance_x = this->x - other.x;
 		double distance_y = this->y - other.y;
 		double distance = sqrt(distance_x * distance_x + distance_y * distance_y);
 		return distance;
 	}
-	void print()
+	void Point::print()
 	{
 		cout << "X = " << x << "\t" << "Y = " << y << endl;
 	}
-
-};
 
 double distance(const Point A, const Point B)
 {
